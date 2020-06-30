42373370dd73246fffb7c22553dedf18d0ca8d53
William Lu <weilianlu@gmail.com>
2020-06-30 14:44:13 +0800

    small error

--- reconstruction.py
+++ reconstruction.py
@@ -1,6 +1,5 @@
 import random
 import math
-import numpy as np
 n = 10 # this is the length of the original string
 q = 1/5 # the deletion probabilty for each bit 
 d = 3 #the precision 
@@ -18,9 +17,11 @@ def function_1(n): # creating string X as a sequence offrom input n.
         i += 1
     return(string_X)
 
+N = math.floor((8*(1+d)* math.log(n))/math.exp(-12*q*n)) # The number of traces we need
+print(N)
 def function_2(N,string_X): # creating N traces of X
     trace_all =[] # list of traces
-    N = math.floor((8*(1+d)* math.log(n))/math.exp(-12*q*n)) # The number of traces we need
+    
     for i in range(N):
         trace = []
         for bit in string_X:
@@ -28,10 +29,11 @@ def function_2(N,string_X): # creating N traces of X
             if y < 1-q:    #determine each bit is remained or not
                 trace.append(string_X[bit])
         trace_all.append(trace)
+    return(trace_all)
 
     return trace_all
 
-def function_3(): #reconstructing the original X string by using traces
+def function_3(trace_all): #reconstructing the original X string by using traces
     probability_list = []
     recovered_X = []
     for j in range(1,math.floor((1-3*q)*n+3*q)):
@@ -41,11 +43,15 @@ def function_3(): #reconstructing the original X string by using traces
                 f += 1
         probability_list.append(f/N) # P(Yj = 1) = probability_list[j-1]
     for i in range(1,n+1):
-        j = (1-3*q)*i + 3*q
+        j = math.floor((1-3*q)*i + 3*q)
         prob_i_j = math.comb(i-1,j-1)*q^(i-j)*(1-q)^j
-        sum_known = int(probability_list[j-1]) - # don't know how to write the sigma sum?
+        summation = int(probability_list[j-1])
+        for l in range(j,i-1):
+            summation = summation - prob_i_j*int(recovered_X[l-1])
+        
 
-        if sum_known >= 3/4*prob_i_j:
+
+        if summation >= 3/4*prob_i_j:
             recovered_X[i-1] = 1
         else:
             recovered_X[i-1] = 0
@@ -60,3 +66,6 @@ def function_4(string_X, recovered_X):
     return difference
             
 print(function_1(10))
+print(function_2(N,string_X))
+print(function_3())
+
